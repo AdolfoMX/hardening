0f6ab662562a6da53d488c557c4bc3698b9be2e0
AdolfoMX <60202428+AdolfoMX@users.noreply.github.com>
2020-11-05 15:41:41 -0500

    Se llego hasta el punto 3.3.9 (Revisar)

--- Parcial2/CIS-Ubuntu-Linux-LTS-Benchmark.sh
+++ Parcial2/CIS-Ubuntu-Linux-LTS-Benchmark.sh
@@ -119,7 +119,7 @@ password root
 journalctl | grep 'protection: active'
 #1.6.2 Ensure address space layout randomization (ASLR) is enabled (Automated)
 # journalctl | grep 'protection: active'
- [[ -n $(grep noexec[0-9]*=off /proc/cmdline) || -z $(grep -E -i ' (pae|nx)
+[[ -n $(grep noexec[0-9]*=off /proc/cmdline) || -z $(grep -E -i ' (pae|nx)
 ' /proc/cpuinfo) || -n $(grep '\sNX\s.*\sprotection:\s' /var/log/dmesg | grep
 -v active) ]] && echo "NX Protection is not active"
 #1.6.2 Ensure address space layout randomization (ASLR) is enabled
@@ -158,11 +158,11 @@ ProcessSizeMax=0
 #RUN
 systemctl daemon-reload
 #####################################################################
-####################################
-1.7 Mandatory Access Control#
+##########################################
+1.7 Mandatory Access Control             #
+#########################################
+1.7.1 Configure AppArmor            #
 #####################################
-1.7.1 Configure AppArmor
-###################3#################
 1.7.1.1 Ensure AppArmor is installed (Automated)
 #Verify that AppArmor is installed:
 # dpkg -s apparmor
@@ -176,11 +176,11 @@ security=apparmor parameters set:
   grep "^\s*linux" /boot/grub/grub.cfg | grep -v "security=apparmor"
 return:nothing
 #Edit /etc/default/grub and add the appermor=1 and security=apparmor parameters to
-the GRUB_CMDLINE_LINUX= line
+# the GRUB_CMDLINE_LINUX= line
 
-GRUB_CMDLINE_LINUX="apparmor=1 security=apparmor"
-Run the following command to update the grub2 configuration:
- update-grub
+# GRUB_CMDLINE_LINUX="apparmor=1 security=apparmor"
+# Run the following command to update the grub2 configuration:
+# update-grub
 #1.7.1.3 Ensure all AppArmor Profiles are in enforce or complain mode
 (Automated)
 #Verify that profiles are loaded, and are in either enforce or
@@ -200,8 +200,8 @@ mode:
 #Set all profiles to enforce mode:
   aa-enforce /etc/apparmor.d/*
 #####################################################################
-############################
-1.8 Warning Banners#
+####################################
+1.8 Warning Banners                #
 ####################################
 #1.8.1.1 Ensure message of the day is configured properly (Automated)
 #verify that the contents match site policy:
@@ -218,44 +218,316 @@ f2 | sed -e 's/"//g'))" /etc/motd
 verify no results are returned:
   grep -E -i "(\\\v|\\\r|\\\m|\\\s|$(grep '^ID=' /etc/os-release | cut -d= -
 f2 | sed -e 's/"//g'))" /etc/issue
-#1.8.1.3 Ensure remote login warning banner is configured properly
-(Automated)
-verify that the contents match site policy:
-  cat /etc/issue.net
+#1.8.1.3 Ensure remote login warning banner is configured properly (Automated)
+#verify that the contents match site policy:
+#  cat /etc/issue.net
 #verify no results are returned:
- grep -E -i "(\\\v|\\\r|\\\m|\\\s|$(grep '^ID=' /etc/os-release | cut -d= -
-f2 | sed -e 's/"//g'))" /etc/issue.net
+grep -E -i "(\\\v|\\\r|\\\m|\\\s|$(grep '^ID=' /etc/os-release | cut -d = -f2 | sed -e 's/"//g'))" /etc/issue.net
 ##Edit the /etc/issue.net file with the appropriate contents according to your site policy,
-remove any instances of \m , \r , \s , \v or references to the OS platform##
-  echo "Authorized uses only. All activity may be monitored and reported." >
-/etc/issue.net
+#remove any instances of \m , \r , \s , \v or references to the OS platform
+echo "Authorized uses only. All activity may be monitored and reported." > /etc/issue.net
 #1.8.1.4 Ensure permissions on /etc/motd are configured (Automated)
-#Verify: Uid and Gid are both 0/root and Access is 644, or
-the file doesn't exist.
-  stat /etc/motd
-  Access: (0644/-rw-r--r--) Uid: ( 0/ root) Gid: ( 0/ root)
-#set permissions on /etc/motd :
-  chown root:root /etc/motd
-  chmod u-x,go-wx /etc/motd
+# Verify: Uid and Gid are both 0/root and Access is 644, or
+# the file doesn't exist.
+# stat /etc/motd
+# Access: (0644/-rw-r--r--) Uid: ( 0/ root) Gid: ( 0/ root)
+# set permissions on /etc/motd :
+chown root:root /etc/motd
+chmod u-x,go-wx /etc/motd
 #Or remove the /etc/motd file:
- rm /etc/motd
+rm /etc/motd
 #1.8.1.5 Ensure permissions on /etc/issue are configured (Automated)
-#Verify Uid and Gid are both 0/root and Access is 644 :
-  stat /etc/issue
-Access: (0644/-rw-r--r--) Uid: ( 0/ root) Gid: ( 0/ root)
-#set permissions on /etc/issue :
-  chown root:root /etc/issue
-  chmod u-x,go-wx /etc/issue
-1.8.1.6 Ensure permissions on /etc/issue.net are configured
-(Automated)
-#verify Uid and Gid are both 0/root and Access is 644 :
-  stat /etc/issue.net
-Access: (0644/-rw-r--r--) Uid: ( 0/ root) Gid: ( 0/ root)
-set permissions on /etc/issue.net :
-  chown root:root /etc/issue.net
-  chmod u-x,go-wx /etc/issue.net
+# Verify Uid and Gid are both 0/root and Access is 644 :
+# stat /etc/issue
+# Access: (0644/-rw-r--r--) Uid: ( 0/ root) Gid: ( 0/ root)
+# set permissions on /etc/issue :
+chown root:root /etc/issue
+chmod u-x,go-wx /etc/issue
+#1.8.1.6 Ensure permissions on /etc/issue.net are configured (Automated)
+# verify Uid and Gid are both 0/root and Access is 644 : stat /etc/issue.net
+# Access: (0644/-rw-r--r--) Uid: ( 0/ root) Gid: ( 0/ root)
+# set permissions on /etc/issue.net :
+chown root:root /etc/issue.net
+chmod u-x,go-wx /etc/issue.net
+#####################################################################
+######################################################################
+#1.9 Ensure updates, patches, and additional security software are   #
+#installed (Manual)                                                  # 
+######################################################################
+#Verify there are no updates or patches to install:
+apt -s upgrade	
+apt dist-upgrade
+#1.10 Ensure GDM is removed or login is configured (Automated)
+# GDM is installed on the system verify that /etc/gdm3/greeter.dconf-defaults file
+# exists and contains the following
+# [org/gnome/login-screen]
+# banner-message-enable=true
+# banner-message-text='<banner message>'
+# disable-user-list=true
+#Run the following command to re-load GDM on the next login or reboot:
+dpkg-reconfigure gdm3
+#############################################################################################################################
+#####################################################################
+#2 Services                                                         #
+#####################################################################
+#2.1 Inetd Services        
+#2.1.1 Ensure xinetd is no installed (Automated)
+#2.1.2 Ensure openbsd-inetd is no installed (Automated)
+apt purge xinetd | apt purge openbsd-inetd
+#####################################################################
+###############################
+#2.2 Special Purpose Servi    #
+###############################
+#2.2.1 Time Synchronization
+#2.2.1.1 Ensure time synchronization is in use (Automated)
+systemctl is-enabled systemd-timesyncd
+#  install chrony or NTP.
+apt install chrony | apt install ntp
+#2.2.1.2 Ensure systemd-timesyncd is configured (Manual)
+apt purge ntp | apt purge chrony
+# Run the following command to enable systemd-timesyncd
+systemctl enable systemd-timesyncd.service
+echo "NTP=0.debian.pool.ntp.org 1.debian.pool.ntp.org 
+Accordence With Local Policy
+FallbackNTP=2.debian.pool.ntp.org 3.debian.pool.ntp.org 
+should be In Accordence With Local Policy
+RootDistanceMax=1" >> /etc/systemd/timesyncd.conf
+# Run the following command to enable systemd-timesyncd
+systemctl start systemd-timesyncd.service
+timedatectl set-ntp true  
+#2.2.1.3 Ensure chrony is configured (Automated)
+apt purge ntp |  systemctl --now mask systemd-timesyncd
+# Configure chrony
+echo "server <remote-server>" >> /etc/chrony/chrony.conf
+echo "user _chrony" >> /etc/chrony/chrony.conf
+#2.2.1.4 Ensure ntp is configured (Automated)
+apt purge chrony |  systemctl --now mask systemd-timesyncd
+# Configure ntp
+echo "restrict -4 default kod nomodify notrap nopeer noquery
+restrict -6 default kod nomodify notrap nopeer noquery" >>  /etc/ntp.conf
+echo "server <remote-server>" >> /etc/ntp.conf
+echo "RUNASUSER=ntp" >> /etc/init.d/ntp
+#2.2.2 Ensure X Window System is not installed (Automated)
+apt purge xserver-xorg*
+#2.2.3 Ensure Avahi Server is not installed (Automated)
+systemctl stop avahi-daaemon.service | systemctl stop avahi-daemon.socket
+apt purge avahi-daemon
+#2.2.4 Ensure CUPS is not installed (Automated)
+apt purge cups
+#2.2.5 Ensure DHCP Server is not installed (Automated)
+# Run the following command to remove isc-dhcp-server
+apt purge isc-dhcp-server
+#2.2.6 Ensure LDAP server is not installed (Automated)
+# Run one of the following commands to remove slapd
+apt purge slapd
+#2.2.7 Ensure NFS is not installed (Automated)
+apt purge rpcbind
+#2.2.8 Ensure DNS Server is not installed (Automated)
+apt purge bind9
+#2.2.9 Ensure FTP Server is not installed (Automated)
+apt purge vsftpd
+#2.2.10 Ensure HTTP server is not installed (Automated)
+apt purge apache2
+#2.2.11 Ensure IMAP and POP3 server are not installed (Automated)
+# Run one of the following commands to remove dovecot-imapd and dovecot-pop3d
+apt purge dovecot-imapd dovecot-pop3d
+#2.2.12 Ensure Samba is not installed (Automated)
+apt purge samba
+#2.2.13 Ensure HTTP Proxy Server is not installed (Automated)
+apt purge squid
+#2.2.14 Ensure SNMP Server is not installed (Automated)
+apt purge snmpd
+#2.2.15 Ensure mail transfer agent is configured for local-only mode (Automated)
+echo "dc_eximconfig_configtype='local'
+dc_local_interfaces='127.0.0.1 ; ::1'
+dc_readhost=''
+dc_relay_domains=''
+dc_minimaldns='false'
+dc_relay_nets=''
+dc_smarthost=''
+dc_use_split_config='false'
+dc_hide_mailname=''
+dc_mailname_in_oh='true'
+dc_localdelivery='mail_spool'" >> /etc/exim4/update-exim4.conf.conf
+# Restart exim4
+systemctl restart exim4
+#2.2.16 Ensure rsync service is not installed (Automated)
+apt purge rsync
+#2.2.17 Ensure NIS Server is not installed (Automated)
+apt purge nis
 #####################################################################
 ####################################
-1.9 Ensure updates, patches, and additional security software are
-installed (Manual)#
+2.3 Service Clients                #
 ####################################
+#2.3.1 Ensure NIS Client is not installed (Automated)
+#to provide the needed information:
+ dpkg -s nis
+#Uninstall nis:
+ apt purge nis
+#2.3.2 Ensure rsh client is not installed (Automated)
+#Verify rsh-client is not installed
+ dpkg -s rsh-client
+#Uninstall rsh:
+ apt purge rsh-client
+#2.3.3 Ensure talk client is not installed (Automated)
+#Verify talk is not installed
+ dpkg -s talk
+#Uninstall talk:
+ apt purge talk
+#2.3.4 Ensure telnet client is not installed (Automated)
+#Verify telnet is not installed.
+ dpkg -s telnet
+#Uninstall telnet:
+ apt purge telnet
+#2.3.5 Ensure LDAP client is not installed (Automated)
+#Verify that ldap-utils is not installed
+ dpkg -s ldap-utils
+#Uninstall ldap-utils:
+ apt purge ldap-utils
+#2.3.6 Ensure RPC is not installed (Automated)
+#verify rpcbind is not installed
+ dpkg -s rpcbind
+#remove rpcbind:
+ apt purge rpcbind
+#2.4 Ensure nonessential services are removed or masked (Manual)
+#Run the following command:
+lsof -i -P -n | grep -v "(ESTABLISHED)"} 
+#remove the package containing the service:
+ apt purge <package_name>
+###If required packages have a dependency:###
+ systemctl --now mask <service_name>
+#############################################################################################################################
+#####################################
+#3 Network Configuration            #
+#####################################
+#3.1 Disable unused network protocols and devices
+#3.1.1 Disable IPv6 (Manual)
+ grep "^\s*linux" /boot/grub/grub.cfg | grep -v "ipv6.disable=1"
+#Edit /etc/default/grub and add ipv6.disable=1 to the GRUB_CMDLINE_LINUX parameters:
+GRUB_CMDLINE_LINUX="ipv6.disable=1"
+#update the grub2 configuration:
+ update-grub
+#3.1.2 Ensure wireless interfaces are disabled (Automated)
+Profile Applicability:
+#Run the following script to verify no wireless interfaces are active on the system:
+ !/bin/bash
+ if command -v nmcli >/dev/null 2>&1 ; then
+  nmcli radio all | grep -Eq '\s*\S+\s+disabled\s+\S+\s+disabled\b' && echo
+ "Wireless is not enabled" || nmcli radio all
+ elif [ -n "$(find /sys/class/net/*/ -type d -name wireless)" ]; then
+  t=0
+  drivers=$(for driverdir in $(find /sys/class/net/*/ -type d -name wireless 
+| xargs -0 dirname); do basename "$(readlink -f
+ "$driverdir"/device/driver)";done | sort -u)
+  for dm in $drivers; do
+  if grep -Eq "^\s*install\s+$dm\s+/bin/(true|false)"
+ /etc/modprobe.d/*.conf; then
+  /bin/true
+  else
+  echo "$dm is not disabled"
+  t=1
+  fi
+  done
+ [[ $t -eq 0 ]] && echo "Wireless is not enabled"
+else
+ echo "Wireless is not enabled"
+fi
+#####################################################################
+#####################################
+#3.2 Network Parameters (Host Only) #
+#####################################
+#3.2.1 Ensure packet redirect sending is disabled (Automated)
+#Run the following commands and verify output matches:
+sysctl net.ipv4.conf.all.send_redirects
+net.ipv4.conf.all.send_redirects = 0
+sysctl net.ipv4.conf.default.send_redirects
+net.ipv4.conf.default.send_redirects = 0
+grep "net\.ipv4\.conf\.all\.send_redirects" /etc/sysctl.conf
+/etc/sysctl.d/*
+net.ipv4.conf.all.send_redirects = 0
+grep "net\.ipv4\.conf\.default\.send_redirects" /etc/sysctl.conf
+/etc/sysctl.d/*
+net.ipv4.conf.default.send_redirects= 0
+Set the following parameters in /etc/sysctl.conf or a /etc/sysctl.d/* file:
+net.ipv4.conf.all.send_redirects = 0
+net.ipv4.conf.default.send_redirects = 0
+#Run the following commands to set the active kernel parameters:
+sysctl -w net.ipv4.conf.all.send_redirects=0
+sysctl -w net.ipv4.conf.default.send_redirects=0
+sysctl -w net.ipv4.route.flush=1
+#3.2.2 Ensure IP forwarding is disabled (Automated)
+#Run the following command and verify output matches:
+sysctl net.ipv4.ip_forward
+net.ipv4.ip_forward = 0
+grep -E -s "^\s*net\.ipv4\.ip_forward\s*=\s*1" /etc/sysctl.conf
+etc/sysctl.d/*.conf /usr/lib/sysctl.d/*.conf /run/sysctl.d/*.conf
+# No value should be returned
+#####################################################################
+##############################################
+#3.3 Network Parameters (Host and Router)    #
+##############################################
+#3.3.1 Ensure source routed packets are not accepted (Automated)
+echo "net.ipv4.conf.all.accept_source_route = 0
+net.ipv4.conf.default.accept_source_route = 0" >> /etc/sysctl.conf
+# Run the following commands to set the active kernel parameters
+sysctl -w net.ipv4.conf.all.accept_source_route=0
+sysctl -w net.ipv4.conf.default.accept_source_route=0
+sysctl -w net.ipv4.route.flush=1
+# IF IPv6 is enabled
+echo "net.ipv6.conf.all.accept_source_route = 0
+net.ipv6.conf.default.accept_source_route = 0" >> /etc/sysctl.conf
+# Run the following commands to set the active kernel parameters
+sysctl -w net.ipv6.conf.all.accept_source_route=0
+sysctl -w net.ipv6.conf.default.accept_source_route=0
+sysctl -w net.ipv6.route.flush=1
+#3.3.2 Ensure ICMP redirects are not accepted (Automated)
+echo "net.ipv4.conf.all.accept_redirects = 0
+net.ipv4.conf.default.accept_redirects = 0" >> /etc/sysctl.conf
+# Run the following commands to set the active kernel parameters
+sysctl -w net.ipv4.conf.all.accept_redirects=0
+sysctl -w net.ipv4.conf.default.accept_redirects=0
+sysctl -w net.ipv4.route.flush=1
+# IF IPv6 is enabled
+echo " net.ipv6.conf.all.accept_redirects = 0
+net.ipv6.conf.default.accept_redirects = 0" >> /etc/sysctl.conf
+sysctl -w net.ipv6.conf.all.accept_redirects=0
+sysctl -w net.ipv6.conf.default.accept_redirects=0
+sysctl -w net.ipv6.route.flush=1
+#3.3.3 Ensure secure ICMP redirects are not accepted (Automated)
+echo "net.ipv4.conf.all.secure_redirects = 0
+net.ipv4.conf.default.secure_redirects = 0" >> /etc/sysctl.conf
+sysctl -w net.ipv4.conf.all.secure_redirects=0
+sysctl -w net.ipv4.conf.default.secure_redirects=0
+sysctl -w net.ipv4.route.flush=1
+#3.3.4 Ensure suspicious packets are logged (Automated)
+echo "net.ipv4.conf.all.log_martians = 1
+net.ipv4.conf.default.log_martians = 1" >> /etc/sysctl.conf
+sysctl -w net.ipv4.conf.all.log_martians=1
+sysctl -w net.ipv4.conf.default.log_martians=1
+sysctl -w net.ipv4.route.flush=1
+#3.3.5 Ensure broadcast ICMP requests are ignored (Automated)
+echo "net.ipv4.icmp_echo_ignore_broadcasts = 1" >> /etc/sysctl.conf
+# Run the following commands to set the active kernel parameters
+sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=1
+sysctl -w net.ipv4.route.flush=1
+#3.3.6 Ensure bogus ICMP responses are ignored (Automated)
+echo "net.ipv4.icmp_ignore_bogus_error_responses = 1" >> /etc/sysctl.conf 
+# Run the following commands to set the active kernel parameters
+sysctl -w net.ipv4.icmp_ignore_bogus_error_responses=1
+sysctl -w net.ipv4.route.flush=1
+#3.3.7 Ensure Reverse Path Filtering is enabled (Automated)
+echo "net.ipv4.conf.all.rp_filter = 1
+net.ipv4.conf.default.rp_filter = 1" >> /etc/sysctl.conf 
+sysctl -w net.ipv4.conf.all.rp_filter=1
+sysctl -w net.ipv4.conf.default.rp_filter=1
+sysctl -w net.ipv4.route.flush=1
+#3.3.8 Ensure TCP SYN Cookies is enabled (Automated)
+echo "net.ipv4.tcp_syncookies = 1" >> /etc/sysctl.conf
+sysctl -w net.ipv4.tcp_syncookies=1
+sysctl -w net.ipv4.route.flush=1
+#3.3.9 Ensure IPv6 router advertisements are not accepted (Automated)
+echo "net.ipv6.conf.all.accept_ra = 0
+net.ipv6.conf.default.accept_ra = 0" >> /etc/sysctl.conf
+sysctl -w net.ipv6.conf.all.accept_ra=0
+sysctl -w net.ipv6.conf.default.accept_ra=0
+sysctl -w net.ipv6.route.flush=1
